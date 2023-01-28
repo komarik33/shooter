38d7c993e4f35b2689399892145262ae8b92fa6f
komarik <kovtuntamani@gmail.com>
2023-01-28 12:44:19 +0000

    add game sprite

--- main.py/maim.py
+++ main.py/maim.py
@@ -2,12 +2,36 @@ import pygame
 import os
 pygame.int()
 
+def file_path(file_name):
+    folder_path = os.pats.abspath(__file__ + "/..")
+    path = os.path.join(folder_path, file_name)
+    return path
+
+
+image_baground = pygame.image.load(file_path(imh))
+pygame.display.set_caption("supershooterfromkomarik")
+image_baground = pygame.transform.scale(image_baground, (WIN_WIDTH, WIN_HEIGHT))
+
 WIN_WIDTH = 700
 WIN_HEIGHT = 500
 FPS = 40
 
 window = pygame.display.set_mode({WIN_WIDTH, WIN_HEIGHT})
 clock = pygame.time.clock
+
+class GameSprite(pygame.sprite.Sprite):
+    def __innit__(self, x, y, WIDTH, HEIGHT, img, speed):
+        super().__innit__()
+        self.image = pygame.image.load(img)
+        self.image = pygqame.transform.scale(self.image, (width, heigt))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.speed = speed
+    
+    def reset(self):
+        window.blit(self.rect.x, self.rect.y)
+
 
 play = True
 game = True
@@ -17,4 +41,7 @@ while game == True:
         game = False
 
     clock.tick(FPS) 
-    pygame.display.update()       
+    pygame.display.update()  
+
+    if play = True:
+        window.blit(img)     
